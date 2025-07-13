d7cd88c528f78a15ab58cebd23f7d18b4964b7e1
RainerMorales <rainermorales213@gmail.com>
2025-07-13 23:51:47 +0800

    first commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Navbars from "@/components/ui/Navbars";
+export default function dashboard() {
+  return (
+    <>
+      <Navbars></Navbars>
+      <main className="max-w-2xl m-auto bg-white/10 h-screen">
+        <div></div>
+      </main>
+    </>
+  );
+}
---
+++ src/components/ui/Navbars.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Home, Search, User } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
+export default function Navbar() {
+  const router = useRouter();
+
+  return (
+    <nav className="fixed bottom-6 left-1/2 transform -translate-x-1/2 w-80 h-14 text-white bg-violet-950 rounded-full flex items-center justify-around shadow-lg z-50">
+      <Button
+        variant="ghost"
+        className="flex flex-col items-center gap-0  text-xs"
+        onClick={() => router.push("/")}
+      >
+        <Home className="w-5 h-5 mb-0.5" />
+        Home
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex flex-col items-center gap-0 text-xs"
+        onClick={() => router.push("/search")}
+      >
+        <Search className="w-5 h-5 mb-0.5" />
+        Search
+      </Button>
+      <Button
+        variant="ghost"
+        className="flex flex-col items-center gap-0 text-xs"
+        onClick={() => router.push("/profile")}
+      >
+        <User className="w-5 h-5 mb-0.5" />
+        Profile
+      </Button>
+    </nav>
+  );
+}
