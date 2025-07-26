9917d240e3d82548936a0135d1b2544545866ccf
RainerMorales <rainermorales213@gmail.com>
2025-07-26 16:42:09 +0800

    change ui in landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,22 +9,22 @@ export default function Home() {
   }
   return (
     <>
-      <main className="h-screen flex flex-col items-center justify-center bg-violet-950/30">
-        <div className=" text-center m-10 space-y-6">
+      <main className="h-screen flex flex-col items-center justify-center bg-color">
+        <div className=" text-center m-10 space-y-2">
           <div>
-            <div className="text-4xl text-white font-bold">
-              Learn Ali<span className="text-violet-600">bata</span>
+            <div className="text-3xl text-white font-bold">
+              Learn Alibata
             </div>
-            <div className="text-xl text-white">ᜎᜒᜀᜇ᜔ᜈ᜔ ᜀᜎᜒᜊᜆ</div>
+            {/* <div className="text-xl text-white">ᜎᜒᜀᜇ᜔ᜈ᜔ ᜀᜎᜒᜊᜆ</div> */}
           </div>
           <div className="text-xs text-zinc-400">
             The Ancient Script of the Philippines
           </div>
         </div>
-        <div className="flex flex-col min-w-50 gap-2 m-20">
+        <div className="flex flex-col min-w-35 gap-2 m-20 ">
           <Button
             onClick={handleClick}
-            className="rounded-full bg-violet-900 hover:bg-violet-700 p-6 cursor-pointer hover:scale-95 active:scale-95 active:bg-violet-700 transition-transform"
+            className="font-bold rounded-full btn-bg-color p-4 cursor-pointer hover:scale-95 active:scale-95 transition-transform"
           >
             Get Started
           </Button>
